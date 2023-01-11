47cf9e194f03cb4cfbf0011da16110d6d12d3eb3
Vllaadd <vlladdiimiir@gmail.com>
2023-01-11 00:51:28 -0500

    typo fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+const [count, setCount] = useState(4);
+const [theme, setTheme] = useState('blue');
+
+function decrementCount(){
+    setCount(prevCount => prevCount - 1);
+}
+
+function incrementCount(){
+  setCount(prevCount => prevCount + 1);
+}
   return (
    <>
-   <button>-</button>
+   <button onClick={decrementCount}>-</button>
    <span>{count}</span>
-   <button>+</button>
+   <span>{theme}</span>
+   <button onClick={incrementCount}>+</button>
    </>
   );
 }
